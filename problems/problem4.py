1e5df638b2b31b86b31a3c6c6456cfd0be35b09c
Andrew Vetovitz <andrewvetovitz@gmail.com>
2021-06-02 01:12:49 -0400

    adding more problems

--- problems/problem4.py
+++ problems/problem4.py
@@ -1,10 +1,30 @@
 import math
 from typing import Set
 
-
-
 def main():
-    pass
+    maxPal = -1
+
+    for i in range(999, 100, -1):
+        for j in range(i, 100, -1):
+            mult = i * j
+
+            if isPalindrome(mult):
+                maxPal = max(maxPal, mult)
+
+    print(maxPal)
+
+def isPalindrome(num):
+    strNum = str(num)
+    left = 0
+    right = len(strNum) - 1
+
+    while left < right:
+        if strNum[left] != strNum[right]:
+            return False
+        
+        left += 1
+        right -= 1
+    
+    return True
 
 main()
-
